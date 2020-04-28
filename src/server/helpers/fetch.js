52bce005e7ee0c07ea540e76abf05433e21cff0b
Sunmin0520 <trymore87@gmail.com>
2020-04-28 12:00:46 +0900

    done helpers

--- src/server/helpers/fetch.js
+++ src/server/helpers/fetch.js
@@ -2,6 +2,21 @@ const https = require('https');
 
 async function retrieveArticle(url) {
   // TODO: retrieve the html string from given url and return as promise
+  return new Promise((resolve, reject)=>{
+    https.get(url,resp => {
+      let data ='';
+      resp
+        .on('data', chunk => {
+          data += chunk;
+        })
+        .on('end', ()=>{
+          console.log(data)
+          resolve(data);
+        })
+        .on('error', err =>
+          reject(err))
+    })
+  })
 }
 
 module.exports = {
--- src/server/helpers/file.js
+++ src/server/helpers/file.js
@@ -2,13 +2,28 @@ const fs = require("fs");
 
 async function writeFile(filename, body) {
   return new Promise((resolve, reject) => {
+    const data = new Uint8Array(Buffer.from(body));
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 저장할 수 있도록 구현하세요.
-  });
-}
+    fs.writeFile(filename, data, (err) => {
+      if(err){
+        reject(err)
+      } else {
+        // eslint-disable-next-line no-console
+        resolve(console.log('The file has been saved!'))
+      }
+    })
+})}
 
 async function readFile(filename) {
   return new Promise((resolve, reject) => {
     // TODO: 특정 파일이름(filename)을 가진 텍스트를 읽을 수 있도록 구현하세요.
+    fs.readFile(filename, "utf8",function(err,data){
+      if(err){
+        reject(err);
+      } else {
+        resolve(data)
+      }
+    })
   });
 }
 
