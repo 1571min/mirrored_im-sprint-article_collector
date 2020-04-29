c36338729644093f82c55c6b5ca0a268cf7387b8
Sunmin0520 <trymore87@gmail.com>
2020-04-29 11:52:33 +0900

    doing modal

--- src/client/App.js
+++ src/client/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import './app.css';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#yourAppElement')
 
 export default class App extends Component {
   state = {
     nowEditing: false,
     nowSaving: false,
+    modalIsOpen : false,
     currentItem: {
       id: null,
       body: null,
@@ -84,6 +100,19 @@ export default class App extends Component {
     this.setState(prevState => ({ nowEditing: !prevState.nowEditing }));
   }
 
+  openModal() {
+    this.setState({modalIsOpen:true})
+  }
+
+  // afterOpenModal() {
+  //   this.setState({})
+  //   subtitle.style.color = '#f00';
+  // }
+
+  closeModal(){
+    this.setState({modalIsOpen:false})
+  }
+
   render() {
     const { sources } = this.state;
     let lis = '';
@@ -96,7 +125,28 @@ export default class App extends Component {
     }
 
     return (
-      <div>
+      <div> 
+        <button onClick={this.openModal}>url추가</button>
+      <Modal
+        isOpen={this.modalIsOpen}
+        //onAfterOpen={this.afterOpenModal}
+        onRequestClose={this.closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+
+        <h2>Hello</h2>
+        <button onClick={this.closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+
         <h3>article collector (for medium blog)</h3>
         <div className={this.state.nowEditing ? 'hidden' : ''}>
           <header>
